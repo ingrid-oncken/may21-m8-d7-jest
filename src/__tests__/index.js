24d751d4bddecc6d670e8021c4489f79e0083f8f
Ingrid Oncken <ingrid.oncken@gmail.com>
2021-10-05 23:51:20 +0100

    teste change

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -65,8 +65,8 @@ describe("Testing the server", () => {
     })
 
     const validProduct = {
-        name: 'iPhone',
-        price: 900
+        name: 'iPhone 14',
+        price: 9900
     }
 
     it("should test that a POST /products is returning us a valid product", async () => {
